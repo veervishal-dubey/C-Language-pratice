dac2ed6348f2f1160c8b148a3bf353425753c87a
Veer Vishal Dubey <veervishaldubey@gmail.com>
2025-11-29 20:01:46 +0530

    Add insert_at_position and delete_target functions

--- insertions_and_deletions_in_doubly_linked_list.c
+++ insertions_and_deletions_in_doubly_linked_list.c
@@ -49,6 +49,56 @@ struct node* insert_end(struct node *f, int data)
     return f;
 }
 
+struct node * insert_at_position(struct node * f , int data, int position)
+{
+    struct node * new, *current=f;
+    new=(struct node *)malloc(sizeof(struct node));
+    new->prev=NULL;
+    new->next=NULL;
+    new->info=data;
+    if (position==1)
+    {
+        if (f==NULL)
+        {
+            f=new;
+            return new;
+        }
+        else
+        {
+           new->next=f;
+           f->prev=new;
+           f=new;
+           return f;
+        }
+    }
+
+    else
+    {
+        for (int i=1;i<position-1 && current!=NULL;i++) //go to the (position -1)th node for zero indexed list.
+        {
+            current=current->next;
+        }
+        if (current==NULL)
+        {
+            printf("Did not insert at position %d as there are not enough nodes in the list.\n",position);
+            free(new);
+            return f;
+        }
+        else
+        {
+            new->next=current->next;
+            new->prev=current;
+            if (current->next!=NULL)
+            {
+                struct node * newnode=current->next;
+                newnode->prev=new;
+            }
+            current->next=new;
+            return f;
+        }
+    }
+}
+
 void display(struct node *f){
     struct node * current=f;
     if (f==NULL)
@@ -61,6 +111,7 @@ void display(struct node *f){
     }
     else
     {
+        printf("NULL <-> ");
         while (current!=NULL)
         {
             printf("%d <-> ",current->info);
@@ -119,6 +170,55 @@ struct node * delete_end(struct node *f)
     }
 }
 
+struct node * delete_target(struct node * f, int target)
+{
+    struct node *current=f, *previous=NULL;
+    if (f==NULL)
+    {
+        printf("The list is empty.\n");
+        return NULL;
+    }
+
+    else if (f->info==target)
+    {
+        f=f->next;
+        f->prev=NULL;
+        free(current);
+        return f;
+    }
+
+    else
+    {
+        while (current!=NULL && current->info!=target)
+        {
+            previous=current;
+            current=current->next;
+        }
+        if (current==NULL)
+        {
+            printf("The target was not found in the list. No deletion occured.\n");
+            return f;
+        }
+
+        else if (current->next==NULL)
+        {
+            previous->next=NULL;
+            free(current);
+            return f;
+        }
+
+        else
+        {
+            struct node *nextnode=current->next;
+            previous->next=nextnode;
+            nextnode->prev=previous;
+            free(current);
+            return f;            
+        }
+
+    }
+}
+
 int main()
 {
     struct node *first=NULL;
@@ -126,9 +226,11 @@ int main()
     first=insert_end(first,1);
     first=insert_end(first,12);
     first=insert_begin(first,23);
+    first=insert_at_position(first,99,3);
     display(first);
     first=delete_end(first);
     first=delete_first(first);
+    first=delete_target(first,99);
     display(first);
     return 0;
 }
